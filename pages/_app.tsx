b8257fda294da2078b6aeb4d7f402dcbf0938723
farhan <farhanrafid97@gmail.com>
2022-06-20 20:49:24 +0700

    switch to apollo server

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import theme from '../theme';
 import { AppProps } from 'next/app';
-
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql,
+} from '@apollo/client';
 function MyApp({ Component, pageProps }: AppProps) {
+  const client = new ApolloClient({
+    uri: 'http://localhost:4000/graphql',
+    credentials: 'include',
+    cache: new InMemoryCache(),
+  });
   return (
     <ChakraProvider resetCSS theme={theme}>
       <Component {...pageProps} />
--- pages/index.tsx
+++ pages/index.tsx
@@ -116,4 +116,4 @@ const Home: NextPage = () => {
   );
 };
 
-export default withUrqlClient(createUrqlClient, { ssr: true })(Home);
+export default Home;
